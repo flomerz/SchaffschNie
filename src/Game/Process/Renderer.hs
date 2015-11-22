5c4d010c2c81ade605c2f7eb4d8785f4c197dd18
Florian Merz <florian.p.m@gmail.com>
2015-11-22 15:35:32 +0100

    animate player

--- src/Game/Process/Renderer.hs
+++ src/Game/Process/Renderer.hs
@@ -36,7 +36,7 @@ instance GameRenderer GameData where
                 where
                     levelText = [text_ ("Level: " ++ show curLvl) 40 & pos_ (20 , y - 60)]
                     triesText = [text_ ("Tries: " ++ show curTries) 40 & pos_ (20, y - 110)]
-            playerShape = [image_ "player" (renderScale, renderScale) (Just ((0, 0), (255, 288))) & pos_ (toupleF (* renderScale) $ pPosition player)]
+            playerShape = [image_ (sprite time "player/run/" 4 2) (renderScale, renderScale) Nothing & pos_ (toupleF (* renderScale) $ pPosition player)]
             renderColumn col@(GameObjectColumn posX _) = render resSettings $ (time, col { oPositionX = posX - curGamePosX })
             columns = filter columnCondition $ currentGameLevel gameData
                 where
